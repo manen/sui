28b42e209ed1e258d12c48eb07cd3696d2018acd
manen <49951816+manen@users.noreply.github.com>
2025-07-07 16:21:30 +0200

    `stage_manager` fully functional

--- stage_manager/src/lib.rs
+++ stage_manager/src/lib.rs
@@ -1,5 +1,63 @@
-use sui::DynamicLayable;
+use std::{cell::RefCell, fmt::Debug, ops::DerefMut, rc::Rc};
 
+use sui::{DynamicLayable, Layable};
+
+/// a command to have the stage manager change the stage to the dynamic layable within
+#[derive(Clone, Debug)]
+pub struct StageChange<'a> {
+	pub to: DynamicLayable<'a>,
+}
+impl<'a> StageChange<'a> {
+	pub fn new<L: Layable + Debug + Clone + 'a>(layable: L) -> Self {
+		Self {
+			to: DynamicLayable::new(layable),
+		}
+	}
+}
+
+#[derive(Clone, Debug)]
+/// manages the stage \
+/// the point is that it can swap out what it's rendering dynamically at runtime,
+/// and any event inside can request a stage change using [StageChange]
 pub struct Stage<'a> {
-	comp: DynamicLayable<'a>,
+	comp: Rc<RefCell<DynamicLayable<'a>>>,
+}
+impl<'a> Stage<'a> {
+	pub fn new<L: Layable + Debug + Clone + 'a>(base_comp: L) -> Self {
+		Self::from_dyn_layable(DynamicLayable::new(base_comp))
+	}
+	pub fn from_dyn_layable(dyn_layable: DynamicLayable<'a>) -> Self {
+		Self {
+			comp: Rc::new(RefCell::new(dyn_layable)),
+		}
+	}
+}
+impl<'a> Layable for Stage<'a> {
+	fn size(&self) -> (i32, i32) {
+		self.comp.borrow().size()
+	}
+	fn render(&self, d: &mut sui::Handle, det: sui::Details, scale: f32) {
+		self.comp.borrow().render(d, det, scale)
+	}
+	fn pass_event(
+		&self,
+		event: sui::core::Event,
+		det: sui::Details,
+		scale: f32,
+	) -> Option<sui::core::ReturnEvent> {
+		let event = self.comp.borrow().pass_event(event, det, scale);
+		match event {
+			Some(event) if event.can_take::<StageChange>() => {
+				let mut change: StageChange =
+					event.take().expect("can_take said yes but couldn't take");
+
+				let mut comp = self.comp.borrow_mut();
+				std::mem::swap(comp.deref_mut(), &mut change.to); // <- this will swap the current stage and the requested stage, making it so the request
+				// and the old stage get dropped at the end of the scope
+
+				None
+			}
+			a => a,
+		}
+	}
 }
